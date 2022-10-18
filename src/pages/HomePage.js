29f23e753797fab3f794f939399d14f7eed2d88c
Vin274 <101485986+Vin274@users.noreply.github.com>
2022-10-18 22:57:43 +1300

    added media keys for quote component

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -11,7 +11,7 @@ export default function HomePage() {
             <Header pageSelected={"Home"} />
             <Hero />
             <AboutServices />
-            {/* <Quote /> */}
+            <Quote />
             {/* <Footer /> */}
         </>
     )
